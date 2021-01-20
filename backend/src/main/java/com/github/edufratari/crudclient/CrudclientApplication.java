d229e8890cca54038defc0f56330b1c5092f996f
edufratari <edufratari@hotmail.com>
2021-01-20 19:05:19 -0300

    Project created

---
+++ backend/src/main/java/com/github/edufratari/crudclient/CrudclientApplication.java
@@ -0,0 +1,13 @@
+package com.github.edufratari.crudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudclientApplication.class, args);
+	}
+
+}
