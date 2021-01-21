5128161c9bbaad3bc2c08bdc08353107c2513c13
edufratari <edufratari@hotmail.com>
2021-01-21 19:59:32 -0300

    Find by id, exception handling and Post client

--- backend/src/main/java/com/github/edufratari/crudclient/controllers/ClientController.java
+++ backend/src/main/java/com/github/edufratari/crudclient/controllers/ClientController.java
@@ -1,12 +1,17 @@
 package com.github.edufratari.crudclient.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.edufratari.crudclient.dto.ClientDTO;
 import com.github.edufratari.crudclient.services.ClientService;
@@ -24,4 +29,16 @@ public class ClientController {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO clientDto = service.findById(id);
+		return ResponseEntity.ok().body(clientDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO clientDto) {
+		clientDto = service.insert(clientDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(clientDto);
+	}
 }
--- backend/src/main/java/com/github/edufratari/crudclient/services/ClientService.java
+++ backend/src/main/java/com/github/edufratari/crudclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.github.edufratari.crudclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.github.edufratari.crudclient.dto.ClientDTO;
 import com.github.edufratari.crudclient.entities.Client;
 import com.github.edufratari.crudclient.repositories.ClientRepository;
+import com.github.edufratari.crudclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -17,9 +19,35 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO clientDto) {
+		Client entity = populateClientEntity(clientDto);
+
+		entity = repository.save(entity);
+		
+		return new ClientDTO(entity);
+	}
+
+	private Client populateClientEntity(ClientDTO clientDto) {
+		Client entity = new Client();
+		entity.setName(clientDto.getName());
+		entity.setCpf(clientDto.getCpf());
+		entity.setIncome(clientDto.getIncome());
+		entity.setBirthDate(clientDto.getBirthDate());
+		entity.setChildren(clientDto.getChildren());
+		return entity;
 	}
 }
